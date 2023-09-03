5bcae5e460fca8251dc0617bbb04d9b517960a6a
Francisco Cidade <franciscocidadehomemjunior@gmail.com>
2023-09-03 19:01:50 -0300

    now it looks good

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<button
+	class="bg-sky-500 text-gray-700 text-sm font-bold rounded-2xl w-full py-3 mt-3 border-b-4 border-b-sky-600"
+	on:click|preventDefault
+	{...$$props}
+>
+	<slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import { intervalDictionary, notes } from '../lib/music';
 	import { page } from '$app/stores';
 	import { getWrapped } from '../lib/array';
+	import Button from '$lib/components/Button.svelte';
 
 	const debug = $page.url.searchParams.get('debug') === '1';
 
@@ -17,6 +18,7 @@
 		notes,
 		notes.indexOf(note) + intervalDistanceInSemitones * directionAsNumber
 	);
+	// TODO! Embaralhar!!!!
 	$: possibleAnswers = [anyOf(except(notes, note, correctNote)), correctNote];
 
 	const check = (guess: string) => {
@@ -33,42 +35,60 @@
 	};
 </script>
 
-<h1>Calculando intervalos</h1>
+<div class="bg-white h-screen flex items-center justify-center p-4">
+	<div class="border-2 border-gray-300 rounded-2xl p-5 h-full flex flex-col justify-between">
+		<h1 class="text-4xl font-bold text-gray-700">Calculando intervalos</h1>
+		<div class="flex justify-between flex-col h-full">
+			<div>
+				<p class="text-2xl font-bold text-gray-700">Qual intervalo praticar?</p>
+				<select
+					class="border-2 border-gray-300 rounded p-1 my-2"
+					bind:value={intervalDistanceInSemitones}
+				>
+					{#each Object.entries(intervalDictionary) as [interval, name]}
+						<option value={Number(interval)}>{name} ({interval} semitons)</option>
+					{/each}
+				</select>
 
-<p>Qual intervalo praticar?</p>
-<select bind:value={intervalDistanceInSemitones}>
-	{#each Object.entries(intervalDictionary) as [interval, name]}
-		<option value={Number(interval)}>{name} ({interval} semitons)</option>
-	{/each}
-</select>
-{#if debug}
-	<p>(Debug) {intervalDistanceInSemitones}</p>
-	<p>(Debug) Nota correta: {correctNote}</p>
-{/if}
+				<select class="border-2 border-gray-300 rounded p-1" bind:value={direction}>
+					<option value="up">Crescente</option>
+					<option value="down">Decrescente</option>
+				</select>
+			</div>
 
-{intervalDistanceInSemitones} semitom(s)
+			<div class="text-7xl font-bold text-gray-700 text-center">{note}</div>
+			<div class="h-40">
+				<div>
+					{#if state === 'guessing'}
+						{#each possibleAnswers as answer}
+							<Button on:click={() => check(answer)} disabled={state !== 'guessing'}
+								>{answer}</Button
+							>
+						{/each}
+					{/if}
+				</div>
 
-<h1>{note}</h1>
+				{#if state !== 'guessing'}
+					<!-- TODO: Better colors -->
+					{#if state === 'right_answer'}
+						<h1 class="text-2xl font-bold text-emerald-500">
+							Correto! A resposta certa é: {correctNote}
+						</h1>
+					{/if}
 
-{#each possibleAnswers as answer}
-	<button on:click|preventDefault={() => check(answer)} disabled={state !== 'guessing'}
-		>{answer}</button
-	>
-{/each}
+					{#if state === 'wrong_answer'}
+						<h1 class="text-2xl font-bold text-rose-500">
+							Errado! A resposta certa é: {correctNote}
+						</h1>
+					{/if}
+					<Button on:click={nextQuestion}>Próximo</Button>
+				{/if}
+			</div>
+		</div>
 
-<select bind:value={direction}>
-	<option value="up">Crescente</option>
-	<option value="down">Decrescente</option>
-</select>
-
-{#if state !== 'guessing'}
-	<button on:click|preventDefault={nextQuestion}>Próximo</button>
-{/if}
-
-{#if state === 'right_answer'}
-	<h1>Acertou! :)</h1>
-{/if}
-
-{#if state === 'wrong_answer'}
-	<h1>Errou! :(</h1>
-{/if}
+		{#if debug}
+			<p>(Debug) {intervalDistanceInSemitones}</p>
+			<p>(Debug) Nota correta: {correctNote}</p>
+		{/if}
+	</div>
+</div>
